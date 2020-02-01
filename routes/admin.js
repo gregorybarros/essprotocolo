1c7d2b10b53765e146df1748551a4a7112c811d4
unknown <gregory.barros@hotmail.com>
2020-01-31 21:55:47 -0300

    Rotas de post

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const mongoose = require('mongoose')
 require('../models/Categoria')
 const Categoria = mongoose.model('categorias')
+require('../models/Postagem')
+const Postagem = mongoose.model('postagens')
 
 router.get('/', (req, res) => {
     res.render('admin/index')
@@ -105,6 +107,111 @@ router.post("/categorias/edit", (req, res) => {
         req.flash("error_msg", "Houve um erro ao editar empresa!")
         res.redirect("/admin/categorias")
     
+    })
+})
+
+router.post("/categorias/deletar", (req, res) => {
+    Categoria.deleteOne({_id: req.body.id}).then(() => {
+        req.flash("success_msg", "Empresa deletada com sucesso!")
+        res.redirect("/admin/categorias")
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao deletar empresa!")
+        res.redirect("/admin/categorias")
+    })
+})
+
+router.get("/postagens", (req, res) => {
+    Postagem.find().lean().populate("empresa").sort({data:"desc"}).then((postagens) => {
+        res.render("admin/postagens", {postagens})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao listar as postagens!")
+        res.redirect("/admin")
+    })
+   
+})
+
+router.get("/postagens/add", (req, res) => {
+    Categoria.find().lean().then((categorias) => {
+        res.render("admin/addpostagens", {categorias})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar formulario!")
+    })
+    
+})
+
+router.post("/postagens/nova", (req, res) => {
+ 
+    let erros = []
+
+    if(req.body.categoria == 0) {
+        erros.push({texto: "Categoria invalida, registre uma categoria"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/addpostagem", {erros: erros})
+    }else {
+        const novaPostagem = {
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo,
+            empresa: req.body.empresa,            
+        }
+
+        new Postagem(novaPostagem).save().then(() => {
+            req.flash("success_msg", "Protocolo criado com sucesso!")
+            res.redirect("/admin/postagens")
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro durante o salvamento do protocolo!")
+            res.redirect("/admin/postagens")
+        })
+
+    }
+})
+
+router.get("/postagens/edit/:id", (req, res) => {
+
+    Postagem.findOne({_id: req.params.id}).lean().then((postagem) => {
+        console.log(postagem.empresa)
+        Categoria.find().lean().then((empresa) => {
+            
+                
+            res.render("admin/editpostagens", {empresa, postagem})
+            console.log(empresa._id)
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao listar empresas!")
+            res.redirect("/admin/postagens")
+        })
+    }).catch((err)=> {
+        req.flash("error_msg", "Houve um erro ao carregar formulario!")
+        res.redirect("/admin/postagens")
+    })
+    
+})
+
+router.post("/postagens/edit", (req, res) => {
+
+    Postagem.findOne({_id: req.body.id}).then((postagem) => {
+
+        postagem.titulo = req.body.titulo
+        postagem.conteudo = req.body.conteudo
+        postagem.empresa = req.body.empresa
+
+        postagem.save().then(() => {
+            req.flash("success_msg", "Protocolo editado com sucesso!")
+            res.redirect("/admin/postagens")
+        }).catch((err) => {
+            req.flash("error_msg", "Erro ao editar protocolo!")
+            res.redirect("/admin/postagens")
+        })
+
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao salvar a edicao!")
+        res.redirect("/admin/postagens")
+    })
+})
+
+router.get("/postagens/deletar/:id", (req, res) => {
+    Postagem.remove({_id: req.params.id}).then(()=> {
+        res.redirect("/admin/postagens")
     })
 })
 
